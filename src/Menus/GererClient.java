6bf6e8ed1ed49e25f778f8046227b8c635acd242
mohamed <mohamed@gmail.com>
2024-12-24 14:54:54 +0100

    i set The Menu Of GererClient And switch cases handler and i implement the Selector interf

--- src/Menus/GererClient.java
+++ src/Menus/GererClient.java
@@ -1,9 +1,60 @@
 package Menus;
 
-public class GererClient {
+
+import Interfaces.OnSelectedListnner;
+import Utils.Input;
+
+
+/* ##############################
+  #         Lets Code By        #
+ #           by Medo          #
+ ##############################  */
+
+public class GererClient implements OnSelectedListnner {
+
+    // CLient Menu  where u can chose what do u want to do
     public static void Menu() {
+        int choix = 0;
+
+                while (choix != 5) {
+                    System.out.println("""
+                            #############################################
+                            #                                           #
+                            #                                           #
+                            #                   Menu                    #
+                            #                                           #
+                            #                                           #
+                            ##############################################
+                            
+                                         1) Ajouter un Client
+                                         2) Afficher les Clients 
+                                         3) Supprimer un Client
+                                         4) Modifier un Client info
+                                         5) Exit
+                            
+                            
+                            """);
+                    System.out.print("   Entrer Votre Choix : ");
+                    choix = Integer.parseInt(Input.GetInput());
+                    OnSelectedListnner onSelectedListnner = new MainMenu();
+                    onSelectedListnner.Select(choix);
 
 
+        }
 
+    }
+
+    @Override
+    public void Select(int Menu) {
+        Services.Client client=new Services.Client();
+        switch (Menu){
+            case 1 -> client.Ajouter();
+            case 2 -> client.Afficher();
+            case 3 -> client.supprimer();
+            case 4 -> client.Modifier();
+            case 5 -> { }
+
+
+        }
     }
 }
