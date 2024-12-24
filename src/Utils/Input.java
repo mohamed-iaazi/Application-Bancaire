9716a918f8fe552f0a6602b11249c750dc9d5f42
mohamed <mohamed@gmail.com>
2024-12-24 11:01:13 +0100

    i create EmailValidtor with regex using pattren and matcher and get inpute function

--- src/Utils/Input.java
+++ src/Utils/Input.java
@@ -1,6 +1,22 @@
 package Utils;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public  class Input {
 
+
+
+    public static boolean EmailValidor(String email){
+      Pattern pattern=Pattern.compile("^[A-Z0-9a-z+.-_]+@[a-zA-Z+.]$");
+      Matcher matcher=pattern.matcher(email);
+      return matcher.matches();
+    }
+
+    public static String GetInput(){
+        Scanner scanner=new Scanner(System.in);
+        return scanner.nextLine();
+    }
 
 }
