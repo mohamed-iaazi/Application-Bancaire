65f194080c7d5904b5ea637bde941edb962a882e
mohamed <mohamed@gmail.com>
2024-12-24 10:13:26 +0100

    create Operations Constructor And  get and set methods

--- src/Menus/Operation.java
+++ src/Menus/Operation.java
@@ -7,5 +7,42 @@ public class Operation {
     private  int Date;
     private  int CompteAssocie;
 
+    public Operation(String type, int montant, int date, int compteAssocie) {
+        Type = type;
+        Montant = montant;
+        Date = date;
+        CompteAssocie = compteAssocie;
+    }
 
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String type) {
+        Type = type;
+    }
+
+    public int getMontant() {
+        return Montant;
+    }
+
+    public void setMontant(int montant) {
+        Montant = montant;
+    }
+
+    public int getDate() {
+        return Date;
+    }
+
+    public void setDate(int date) {
+        Date = date;
+    }
+
+    public int getCompteAssocie() {
+        return CompteAssocie;
+    }
+
+    public void setCompteAssocie(int compteAssocie) {
+        CompteAssocie = compteAssocie;
+    }
 }
