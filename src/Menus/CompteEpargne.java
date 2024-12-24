93564868a92851629091fec48872541900235da3
mohamed <mohamed@gmail.com>
2024-12-24 10:13:16 +0100

    create CompteEpargne Constructor And  get and set methods

--- src/Menus/CompteEpargne.java
+++ src/Menus/CompteEpargne.java
@@ -3,4 +3,16 @@ package Menus;
 public class CompteEpargne {
 
     private  int tauxInteret;
+
+    public CompteEpargne(int tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
+
+    public int getTauxInteret() {
+        return tauxInteret;
+    }
+
+    public void setTauxInteret(int tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
 }
