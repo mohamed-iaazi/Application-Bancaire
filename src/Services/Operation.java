ed97b9c659c5693ac00d45a3a54f170f48f1c46d
mohamed <mohamed@gmail.com>
2024-12-24 17:26:47 +0100

    i create virment func and retrait func and depots func

--- src/Services/Operation.java
+++ src/Services/Operation.java
@@ -1,4 +1,13 @@
 package Services;
 
 public class Operation {
+
+    public static void Virment() {
+    }
+
+    public static  void Retrait() {
+    }
+
+    public static void Depots() {
+    }
 }
