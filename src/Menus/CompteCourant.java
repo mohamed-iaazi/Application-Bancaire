703898bc917d31ade341552d0708e98e458bc111
mohamed <mohamed@gmail.com>
2024-12-24 10:14:25 +0100

    create CompteCourant Constructor And  get and set methods

--- src/Menus/CompteCourant.java
+++ src/Menus/CompteCourant.java
@@ -2,4 +2,16 @@ package Menus;
 
 public class CompteCourant {
     private  int FraisBancaires;
+
+    public CompteCourant(int fraisBancaires) {
+        FraisBancaires = fraisBancaires;
+    }
+
+    public int getFraisBancaires() {
+        return FraisBancaires;
+    }
+
+    public void setFraisBancaires(int fraisBancaires) {
+        FraisBancaires = fraisBancaires;
+    }
 }
