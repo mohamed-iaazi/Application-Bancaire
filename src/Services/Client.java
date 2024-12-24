5bdc05390a854ebde225d1d6cd67f9a4f29fd8b9
mohamed <mohamed@gmail.com>
2024-12-24 18:03:05 +0100

    get input in ajouter func

--- src/Services/Client.java
+++ src/Services/Client.java
@@ -1,8 +1,10 @@
 package Services;
 
 import Interfaces.Crud;
+import Utils.Input;
 
 public class Client implements Crud {
+
 
     @Override
     public void supprimer() {
@@ -11,6 +13,16 @@ public class Client implements Crud {
 
     @Override
     public void Afficher() {
+        String name,Last,Email,Address;
+
+        System.out.print("   Enter The Name : ");
+         name= Input.GetInput();
+        System.out.print("   Enter The Last  Name : ");
+        Last= Input.GetInput();
+        System.out.print("   Enter The Email : ");
+        Email= Input.GetInput();
+        System.out.print("   Enter The Adress : ");
+        Address= Input.GetInput();
 
     }
 
