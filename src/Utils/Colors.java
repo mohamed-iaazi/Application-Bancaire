afe5a54cedbba1a1e98746a3c1ff17f5d56d598e
mohamed <mohamed@gmail.com>
2024-12-24 15:56:50 +0100

    I create Colors Utils Class

---
+++ src/Utils/Colors.java
@@ -0,0 +1,10 @@
+package Utils;
+
+public class Colors {
+   public final  static String Red="\u001B[31m";
+    public final  static String Green="\u001B[32m";
+    public final  static String Yellow="\u001B[33m";
+    public final  static String Reset="\u001B[37m";
+    public final  static String Blue="\u001B[34m";
+
+}
