99549093d20c72a155e6c00cb5a6cadca15dd5d8
mohamed <mohamed@gmail.com>
2024-12-24 10:07:42 +0100

    create Class Client

---
+++ src/Menus/Client.java
@@ -0,0 +1,13 @@
+package Menus;
+
+public class Client {
+    private  int Id;
+    private  String Name;
+    private String Prenom;
+    private String Email;
+    private  String Adresse;
+
+
+
+
+}
