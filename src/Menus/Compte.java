09664786166cc121e72ff8f2b0b62a6e2353b857
mohamed <mohamed@gmail.com>
2024-12-24 10:10:54 +0100

    create Compte Constructor And  get and set methods

--- src/Menus/Compte.java
+++ src/Menus/Compte.java
@@ -6,6 +6,33 @@ abstract  class   Compte {
     private  int Solde;
     private  Client proprietaire;
 
+    public Compte(int numero, int solde, Client proprietaire) {
+        Numero = numero;
+        Solde = solde;
+        this.proprietaire = proprietaire;
+    }
 
+    public int getNumero() {
+        return Numero;
+    }
 
+    public void setNumero(int numero) {
+        Numero = numero;
+    }
+
+    public int getSolde() {
+        return Solde;
+    }
+
+    public void setSolde(int solde) {
+        Solde = solde;
+    }
+
+    public Client getProprietaire() {
+        return proprietaire;
+    }
+
+    public void setProprietaire(Client proprietaire) {
+        this.proprietaire = proprietaire;
+    }
 }
