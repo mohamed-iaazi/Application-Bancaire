fa257d7f12a8d9cf9f3bd4ffcaeb185a6df833dd
mohamed <mohamed@gmail.com>
2024-12-24 10:10:04 +0100

    create Client Constructor And  get and set methods

--- src/Menus/Client.java
+++ src/Menus/Client.java
@@ -7,7 +7,51 @@ public class Client {
     private String Email;
     private  String Adresse;
 
+    public Client(int id, String name, String prenom, String email, String adresse) {
+        Id = id;
+        Name = name;
+        Prenom = prenom;
+        Email = email;
+        Adresse = adresse;
+    }
 
+    public int getId() {
+        return Id;
+    }
 
+    public void setId(int id) {
+        Id = id;
+    }
 
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getPrenom() {
+        return Prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        Prenom = prenom;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getAdresse() {
+        return Adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        Adresse = adresse;
+    }
 }
