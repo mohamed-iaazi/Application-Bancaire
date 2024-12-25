cfcd3517574919f9a3a04d0597c9172eb499d48f
mohamed <mohamed@gmail.com>
2024-12-25 11:29:40 +0100

    fixing Id size to add +1 for id in each time

--- src/Services/Client.java
+++ src/Services/Client.java
@@ -1,9 +1,12 @@
 package Services;
 
 import Interfaces.Crud;
+import Utils.Colors;
 import Utils.Input;
 
 import java.util.ArrayList;
+
+import static Utils.Colors.*;
 
 public class Client implements Crud {
     private static ArrayList<Model.Client> ClientList=new ArrayList<>();
@@ -17,30 +20,48 @@ public class Client implements Crud {
 
     @Override
     public void Afficher() {
-
-
-
+        if (!ClientList.isEmpty()) {
+            for (Model.Client client : ClientList) {
+                System.out.print(Green+"\n\n-------------Id "+Reset+client.getId()+Green+" -------------\n"+Reset
+                                +Yellow+ "\n      Nome : "+ Reset +client.getName()
+                                +Yellow+ "\n      Prenom : "+Reset+client.getPrenom()
+                                +Yellow+ "\n      Email : "+Reset+client.getEmail()
+                                +Yellow+ "\n      Adress : "+Reset+client.getAdresse()
+                                +Green+  "\n-------------------------------"+Reset
+                 );
+            }
+        }
+        else {
+            System.out.println(Red+"\n\n   Aucune Client à afficher  "+Reset);
+        }
     }
 
     @Override
     public void Ajouter() {
         boolean valid=false;
         String name,Last,Email,Address;
-        System.out.print("   Enter The Name : ");
+        System.out.print("   Entrer Le Nome : ");
         name= Input.GetInput();
-        System.out.print("   Enter The Last  Name : ");
+        System.out.print("   Entrer Prenom : ");
         Last= Input.GetInput();
 
         while (!valid) {
-            System.out.print("   Enter The Email : ");
+            System.out.print("   Entrer  Email : ");
             Email = Input.GetInput();
             if (Input.EmailValidor(Email)) {
                 valid=true;
-                System.out.print("   Enter The Adress : ");
+                System.out.print("   Entrer  Adress : ");
                 Address= Input.GetInput();
-                ClientList.add(new Model.Client(Id+1,name,Last,Email,Address));
+                if (!ClientList.isEmpty()) {
+                    Id = ClientList.getLast().getId() + 1;
+                }
+                else {
+                    Id=Id+1;
+                }
+
+                ClientList.add(new Model.Client(Id,name,Last,Email,Address));
             } else if (!Input.EmailValidor(Email)) {
-                System.out.println("  Envalid Email");
+                System.out.println(Red+"   Email Non Valide  "+Reset);
 
             }
 
