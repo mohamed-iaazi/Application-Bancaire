951988b9c18e3a5fa0b398344d4a614963491b2a
mohamed <mohamed@gmail.com>
2024-12-25 12:57:21 +0100

    i set the supprimer function est modifier

--- src/Services/Client.java
+++ src/Services/Client.java
@@ -12,10 +12,46 @@ public class Client implements Crud {
     private static ArrayList<Model.Client> ClientList=new ArrayList<>();
     private static int Id;
 
+    public static ArrayList<Model.Client> getClientList() {
+        return ClientList;
+    }
+
+    public static void setClientList(ArrayList<Model.Client> clientList) {
+        ClientList = clientList;
+    }
 
     @Override
     public void supprimer() {
+        boolean available=false;
+        if (!ClientList.isEmpty()) {
+        while (!available) {
 
+                System.out.print(Yellow + "\n\n   Entrer Le Id De Client Pour Supprimer :  " + Reset);
+
+                for (int i=0;i<ClientList.size();i++) {
+
+                    if (Integer.parseInt(Input.GetInput()) == ClientList.get(i).getId()) {
+                        Id=i;
+
+                        available=true;
+                    }
+
+
+                }
+
+                if (available){
+                    ClientList.remove(Id);
+                    System.out.print(Green + "\n    Le client a été supprimé avec succès " + Reset);
+                }
+                else {
+                    System.out.println(Red + "\n\n      L'identifiant ne correspond à aucun client  " + Reset);
+
+                }
+            }
+        }
+        else {
+            System.out.println(Red + "\n\n   Aucune Client à Supprimer  \n" + Reset);
+        }
     }
 
     @Override
@@ -70,6 +106,59 @@ public class Client implements Crud {
 
     @Override
     public void Modifier() {
+        boolean available=false;
+        boolean valid=false;
 
+        if (!ClientList.isEmpty()) {
+            while (!available) {
+                System.out.print(Yellow + "\n\n   Entrer Le Id De Client Pour Modifier :  " + Reset);
+                int Id=Integer.parseInt(Input.GetInput());
+                for (int i = 0; i < ClientList.size(); i++) {
+
+                    if (Id == ClientList.get(i).getId()) {
+
+                        String name,Last,Email,Address;
+                        System.out.print("   Entrer Le Nome : ");
+                        name= Input.GetInput();
+                        System.out.print("   Entrer Prenom : ");
+                        Last= Input.GetInput();
+
+                        while (!valid) {
+                            System.out.print("   Entrer  Email : ");
+                            Email = Input.GetInput();
+                            if (Input.EmailValidor(Email)) {
+                                valid=true;
+                                available=true;
+                                System.out.print("   Entrer  Adress : ");
+                                Address= Input.GetInput();
+                                ClientList.set(i,new Model.Client(Id,name,Last,Email,Address));
+                            } else if (!Input.EmailValidor(Email)) {
+                                System.out.println(Red+"   Email Non Valide  "+Reset);
+
+                            }
+
+                        }
+                    }
+
+
+                }
+
+                if (available){
+                    System.out.print(Green + "   Le client a été Modifier avec succès " + Reset);
+                }
+                else {
+                    System.out.println(Red + "\n\n      L'identifiant ne correspond à aucun client  " + Reset);
+
+                }
+
+            }
+        }
+        else {
+            System.out.println(Red + "\n\n   Aucune Client à Modifier  \n" + Reset);
+        }
     }
+
+
 }
+
+
