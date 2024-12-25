a7b910942cbf2b2acee7e5df158e27e0c68c45a7
mohamed <mohamed@gmail.com>
2024-12-25 10:32:05 +0100

    i add validtor for email and i set the data in arraylist

--- src/Services/Client.java
+++ src/Services/Client.java
@@ -3,7 +3,11 @@ package Services;
 import Interfaces.Crud;
 import Utils.Input;
 
+import java.util.ArrayList;
+
 public class Client implements Crud {
+    private static ArrayList<Model.Client> ClientList=new ArrayList<>();
+    private static int Id;
 
 
     @Override
@@ -13,22 +17,34 @@ public class Client implements Crud {
 
     @Override
     public void Afficher() {
-        String name,Last,Email,Address;
 
-        System.out.print("   Enter The Name : ");
-         name= Input.GetInput();
-        System.out.print("   Enter The Last  Name : ");
-        Last= Input.GetInput();
-        System.out.print("   Enter The Email : ");
-        Email= Input.GetInput();
-        System.out.print("   Enter The Adress : ");
-        Address= Input.GetInput();
+
 
     }
 
     @Override
     public void Ajouter() {
+        boolean valid=false;
+        String name,Last,Email,Address;
+        System.out.print("   Enter The Name : ");
+        name= Input.GetInput();
+        System.out.print("   Enter The Last  Name : ");
+        Last= Input.GetInput();
 
+        while (!valid) {
+            System.out.print("   Enter The Email : ");
+            Email = Input.GetInput();
+            if (Input.EmailValidor(Email)) {
+                valid=true;
+                System.out.print("   Enter The Adress : ");
+                Address= Input.GetInput();
+                ClientList.add(new Model.Client(Id+1,name,Last,Email,Address));
+            } else if (!Input.EmailValidor(Email)) {
+                System.out.println("  Envalid Email");
+
+            }
+
+        }
     }
 
     @Override
