7004be6f4a947014e4b55c99eb00d52962e91d22
mohamed <mohamed@gmail.com>
2024-12-26 10:00:14 +0100

    i set display function

--- src/database/Historique.java
+++ src/database/Historique.java
@@ -1,6 +1,12 @@
 package database;
 
+import static Utils.Colors.Green;
+import static Utils.Colors.Reset;
+import static Utils.Colors.Yellow;
+
 import Model.Operation;
+import Services.Client;
+import Services.Compte;
 
 import java.util.ArrayList;
 
@@ -19,5 +25,17 @@ public class Historique {
 
     public static void Afficher() {
 
+        if (!Opereation.isEmpty()) {
+            for (Operation client : Opereation) {
+                System.out.print(Green+"\n\n-------------Id "+Reset+(client.getCompteAssocie()+1)+Green+" -------------\n"+Reset
+                        +Yellow+ "\n      Account Type : "+ Reset +client.getType()
+                        +Yellow+ "\n      Montant : "+Reset+client.getMontant()
+                        +Yellow+ "\n      Creation Date : "+Reset+client.getDate()
+                        +Yellow+ "\n      Compte Numero : "+Reset+"Ma 64000000"
+                        + Compte.getCompteArrayList().get(client.getCompteAssocie()).getNumero()
+                        +Green+  "\n-------------------------------"+Reset
+                );
+            }
+        }
     }
 }
