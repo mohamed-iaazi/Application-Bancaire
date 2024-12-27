3a1dbcaa1a55e7dd8ab01177a19e77f220f3f638
mohamed <mohamed@gmail.com>
2024-12-27 10:24:09 +0100

    i  set condiction to check if Client List is Empty In The Global Menu of Gerer Oprataion Instead Of cpy it many Time you canot do any oeration

--- src/Services/Operation.java
+++ src/Services/Operation.java
@@ -8,35 +8,36 @@ import static Utils.Colors.*;
 public class Operation {
 
     public static void Virment() {
-        boolean error=false;
-        System.out.print("   Entrez l Nome client pour l'expéditeur : ");
-        String senderNome= Input.GetInput();
-        System.out.print("   Entrez l Nome client pour le destinataire : ");
-        String receiverNome= Input.GetInput();
-        if (Input.ClientIsAvailable(senderNome)>=0 && Input.ClientIsAvailable(receiverNome)>=0){
-            System.out.print("   Entrez le montant  : ");
-            do {
-                try {
-                    int montant = Integer.parseInt(Input.GetInput());
-                    if (montant<=0){
-                        error=true;
-                        System.out.println(Red+"   Montant Non Valide  "+Reset);
-                    }
-                    else {
+            boolean error = false;
+            System.out.print("   Entrez l Nome client pour l'expéditeur : ");
+            String senderNome = Input.GetInput();
+            System.out.print("   Entrez l Nome client pour le destinataire : ");
+            String receiverNome = Input.GetInput();
+            if (Input.ClientIsAvailable(senderNome) >= 0 && Input.ClientIsAvailable(receiverNome) >= 0) {
+                System.out.print("   Entrez le montant  : ");
+                do {
+                    try {
+                        int montant = Integer.parseInt(Input.GetInput());
+                        if (montant <= 0) {
+                            error = true;
+                            System.out.println(Red + "   Montant Non Valide  " + Reset);
+                        } else {
 
-                        if (DecreaseVairmentMontant(montant,Input.ClientIsAvailable(senderNome))) {
-                            AddVairmentMontant(montant, Input.ClientIsAvailable(receiverNome));
+                            if (DecreaseVairmentMontant(montant, Input.ClientIsAvailable(senderNome))) {
+                                AddVairmentMontant(montant, Input.ClientIsAvailable(receiverNome));
+                            }
                         }
+                    } catch (NumberFormatException e) {
+                        System.out.println(Red + "   Montant Non Valide  " + Reset);
+                        error = true;
                     }
-                } catch (NumberFormatException e) {
-                    System.out.println(Red+"   Montant Non Valide  "+Reset);
-                    error=true;
-                }
-            }while (error);
-        }
-        else {
+                } while (error);
+            } else {
+                System.out.print(Red+"\n\n   Aucun client associé à ce nom "+Reset);
 
-        }
+            }
+
+
     }
 
     private static void AddVairmentMontant(int montant,int Id) {
